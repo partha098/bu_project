d056feeed3166848a1db3060ed1a51ba30d96827
Partha <cm.partha@gmail.com>
2022-10-16 15:14:09 +0530

    test

---
+++ admin/inc/menu.php
@@ -0,0 +1,73 @@
+<ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+
+            <!-- Sidebar - Brand -->
+            <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+                <div class="sidebar-brand-icon rotate-n-15">
+                    <i class="fas fa-laugh-wink"></i>
+                </div>
+                <div class="sidebar-brand-text mx-3">SB Admin <sup>2</sup></div>
+            </a>
+
+            <!-- Divider -->
+            <hr class="sidebar-divider my-0">
+
+            <!-- Nav Item - Dashboard -->
+            <li class="nav-item">
+                <a class="nav-link" href="master.php">
+                    <i class="fas fa-fw fa-tachometer-alt"></i>
+                    <span>Dashboard</span></a>
+            </li>
+
+           
+
+            <!-- Nav Item - Pages Collapse Menu -->
+            <li class="nav-item">
+                <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo"
+                    aria-expanded="true" aria-controls="collapseTwo">
+                    <i class="fas fa-fw fa-cog"></i>
+                    <span>Event Management</span>
+                </a>
+                <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+                    <div class="bg-white py-2 collapse-inner rounded">
+                      
+                        <a class="collapse-item" href="add-event.php">Add Event</a>
+                        <a class="collapse-item" href="list-event.php">List Events</a>
+                  
+                        <a class="collapse-item" href="past-event.php">Past Events</a>
+                    </div>
+                </div>
+            </li>
+
+
+            <li class="nav-item">
+                <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#gallery"
+                    aria-expanded="true" aria-controls="gallery">
+                    <i class="fas fa-fw fa-cog"></i>
+                    <span>Gallery Management</span>
+                </a>
+                <div id="gallery" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+                    <div class="bg-white py-2 collapse-inner rounded">
+                      
+                        <a class="collapse-item" href="add-gallery.php">Add Gallery</a>
+                        <a class="collapse-item" href="list-gallery.php">List Gallery</a>
+                    </div>
+                </div>
+            </li>
+
+         
+
+
+            <!-- Nav Item - Pages Collapse Menu -->
+            
+
+           
+
+            <!-- Divider -->
+            <hr class="sidebar-divider d-none d-md-block">
+
+            <!-- Sidebar Toggler (Sidebar) -->
+            <div class="text-center d-none d-md-inline">
+                <button class="rounded-circle border-0" id="sidebarToggle"></button>
+            </div>
+
+        </ul>
